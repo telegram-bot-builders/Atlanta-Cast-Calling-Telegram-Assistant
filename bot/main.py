f973e5389af97de7037b1d153643335722f85ab7
nate2427 <natebaker2427@gmail.com>
2024-06-17 10:58:26 -0400

    updated the bot to point to the channel.

--- bot/main.py
+++ bot/main.py
@@ -29,6 +29,7 @@ def poll_backstage_for_casting_calls():
         run_info = browse_ai.retrieve_task(run_info["result"]["robotId"], run_info["result"]["id"])
         time.sleep(15)
         pprint.pprint(run_info["result"]["status"])
+    pprint.pprint(run_info["result"]["status"])
     # grab the list of Casting Calls from result -> status -> capturedLists -> Casting Calls
     castingCalls = run_info["result"]["capturedLists"]["Casting Calls"]
     # create a list of all the Detail Link
@@ -46,10 +47,12 @@ async def send_message_to_all_users(filtered_casting_calls: list):
     print("Sending messages to all users...\nNumber of casting calls: ", len(filtered_casting_calls))
     for castingCall in filtered_casting_calls:
         # send a message to all users with the casting call information
+        if castingCall["Title"] == None:
+            continue
         await send_to_telegram(castingCall)
         time.sleep(3)
 
-async def run_polling_and_send_messages(context: ContextTypes.DEFAULT_TYPE):
+async def run_polling_and_send_messages(context: ContextTypes.DEFAULT_TYPE = None):
     # Implement this function to run the polling and sending messages
     filtered_casting_calls = poll_backstage_for_casting_calls()
     await send_message_to_all_users(filtered_casting_calls)
@@ -77,7 +80,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("Start Polling", callback_data='start_polling')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     # print chat_id
-    # print(update.message.chat_id)
+    print(update.message.chat_id)
     await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 async def stop_pooling(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -98,19 +101,13 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         job = context.job_queue.run_repeating(run_polling_and_send_messages, interval=300)
         await query.edit_message_text(text="Polling started!")
 
-def main():
-    app = Application.builder().token(TOKEN).build()
+async def main():
+    await run_polling_and_send_messages()
 
-    start_handler = CommandHandler('start', start)
-    stop_pooling_handler = CommandHandler('stop_pooling', stop_pooling)
-    button_handler = CallbackQueryHandler(button)
 
-    app.add_handler(start_handler)
-    app.add_handler(stop_pooling_handler)
-    app.add_handler(button_handler)
 
-    print("Bot initialized...")
-    app.run_polling()
 
 if __name__ == '__main__':
-    main()
+    # run main async function
+    import asyncio
+    asyncio.run(main())
